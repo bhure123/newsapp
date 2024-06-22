fd95e287cc423f17e679ed88a3acdb4b2877f86e
Vyankat Bhure <128737584+bhure123@users.noreply.github.com>
2024-06-23 01:40:26 +0530

    Updates

---
+++ precache-manifest.563b972cf82b78796787758751c71c6d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98e32b0b4796215a87e517fa79c27dd4",
+    "url": "/bhure123/newsapp/index.html"
+  },
+  {
+    "revision": "4250c9ae84a4d4a4692d",
+    "url": "/bhure123/newsapp/static/css/main.be0c93d8.chunk.css"
+  },
+  {
+    "revision": "77b9c99d15df571abaed",
+    "url": "/bhure123/newsapp/static/js/2.32846cb6.chunk.js"
+  },
+  {
+    "revision": "c9c5f12326e730f6171c9e7086115877",
+    "url": "/bhure123/newsapp/static/js/2.32846cb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "575c71d9a484fc7ab052",
+    "url": "/bhure123/newsapp/static/js/3.d1bbd77a.chunk.js"
+  },
+  {
+    "revision": "4250c9ae84a4d4a4692d",
+    "url": "/bhure123/newsapp/static/js/main.8fd1d0a5.chunk.js"
+  },
+  {
+    "revision": "3302c9f0da18a3d2c674",
+    "url": "/bhure123/newsapp/static/js/runtime-main.caa940fa.js"
+  },
+  {
+    "revision": "a1f67e956d9d335e63bbb932c200e7a7",
+    "url": "/bhure123/newsapp/static/media/loading.a1f67e95.gif"
+  }
+]);
